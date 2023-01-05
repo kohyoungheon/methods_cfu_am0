3903dbafb4af087ecdb7572052325670a56fdd8b
Young Koh <youngheon6@gmail.com>
2023-01-05 12:52:56 -0700

    finish define_and_id.rb

--- define_and_id.rb
+++ define_and_id.rb
@@ -11,17 +11,31 @@
 
 
 "Hello World".include?("Hello")
+# The #include? method is being called on the string, "Hello World".
+# The method requires a string arguement to be passed in. It will look to see if the arguement exists in the string that called the function.
+# Returns a boolean. In this case, true.
+
+
 
 
 "Hello World".end_with?("Hello")
+# The #end_with? method is being called on "Hello World"
+# The method requires a string argument to be passed. The method checks if the caller string ends in the arguement string.
+# Returns a boolean. In this case, false.
 
 
 "Hello World".end_with?("rld")
-
+# The #end_with? method is being called on "Hello World" , same as the first
+# The method requires a string argument to be passed. The method checks if the caller string ends in the arguement string.
+# Returns a boolean. In this case, true.
 
 12.even?
-
+# The #even? method is being called on the integer 12.
+# The method needs to be called on an integer object but has no parameters and takes no arguements. Checks to see if the integer is even.
+# Returns a boolean, in this case, true.
 
 18.next
-
+# The #next method is being called on the integer 18.
+# The method needs to be called on an integer object but has no parameters and takes no arguements. Returns the consecutive integer after the caller.
+# Returns the consecutive number, in this case, 19.
 
